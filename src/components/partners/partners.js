86b6dba9ac09a1391c495e10e13fdf14abb4c40a
jonatas <jonatasgeek@gmail.com>
2021-08-03 18:53:51 -0300

    Adiciona os links para as imagens de parceria

--- src/components/partners/partners.js
+++ src/components/partners/partners.js
@@ -9,9 +9,15 @@ export default function Partners(){
             <div className="container">
                 <h2><b>Partners</b></h2>
                 <div className="partners-img">
-                    <img src={unbLogo} className="unb-logo" alt="Universidade de Brasília" />
-                    <img src={fapLogo} alt="Fundação de Apoio à Pesquisa no Distrito Federal" />
-                    <img src={finatecLogo} alt="Fundação de Empreendimentos Científicos e Tecnológicos"/>
+                    <a href="https://www.unb.br/">
+                        <img src={unbLogo} className="unb-logo" alt="Universidade de Brasília" />
+                    </a>
+                    <a href="http://www.fap.df.gov.br/">
+                        <img src={fapLogo} alt="Fundação de Apoio à Pesquisa no Distrito Federal" />
+                    </a>
+                    <a href="http://www.fap.df.gov.br/">
+                        <img src={finatecLogo} alt="Fundação de Empreendimentos Científicos e Tecnológicos"/>
+                    </a>
                 </div>
             </div>
         </section>

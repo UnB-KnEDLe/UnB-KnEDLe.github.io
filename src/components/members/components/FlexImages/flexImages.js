c1a5051168ae950bb68f531d625547f1946a9bec
jonatas <jonatasgeek@gmail.com>
2021-08-03 22:53:41 -0300

    Seção de membros atualizada

--- src/components/members/components/FlexImages/flexImages.js
+++ src/components/members/components/FlexImages/flexImages.js
@@ -1,37 +1,61 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes, faEnvelope, faWindowMaximize } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faEnvelope, faWindowMaximize, faIdBadge } from '@fortawesome/free-solid-svg-icons'
 import githubIcon from '../../../../assets/img/iconGithub.png'
 import linkedinIcon from '../../../../assets/img/iconLinkedIn.png'
 
 import { useState } from 'react';
+
+import './flexImages.css'
 
 function FlexImage(props) {
     var item = props.item;
 
     const [showInfo, setShowInfo] = useState(false);
 
-    return (
-        <div className={ showInfo ? "flex-image active" : "flex-image" }>
-            <div onClick={ setShowInfo } className="flex-image-info">
-                <img src={item.avatar} alt={item.name}/>
-                <h4>{item.name}</h4>
+    if(showInfo) {
+        return (
+            <>
+            <div className="flex-image">
+                <div onClick={ setShowInfo } className="flex-image-info">
+                    <img src={item.avatar} alt={item.name}/>
+                    <h4>{item.name}</h4>
+                </div>
             </div>
-            <div className="flex-image-description">
-                <FontAwesomeIcon
-                    onClick={ () => setShowInfo(false) }
-                    className="close-image"
-                    size="lg"
-                    icon={faTimes}
-                />
-                {item.descr?.resume ? <h3>{item.descr?.resume}</h3> : null }
-                {item.descr?.email ? <p><FontAwesomeIcon size="lg" icon={ faEnvelope }/> {item.descr?.email}</p> : null }
-                {item.descr?.page ? <p><FontAwesomeIcon size="lg" icon={ faWindowMaximize }/> {item.descr?.email}</p> : null }
-                {item.descr?.github ? <p><img src={githubIcon} className="members-icon" alt="github"/> {item.descr?.github}</p> : null }
-                {item.descr?.linkedin ? <p><img src={linkedinIcon} className="members-icon" alt="linkedin"/> {item.descr?.linkedin}</p> : null }
+            <div className="flex-image active">
+                <div onClick={ setShowInfo } className="flex-image-info">
+                    <img src={item.avatar} alt={item.name}/>
+                    <h4>{item.name}</h4>
+                </div>
+                <div className="flex-image-description">
+                    <FontAwesomeIcon
+                        onClick={ () => setShowInfo(false) }
+                        className="close-image"
+                        size="lg"
+                        icon={faTimes}
+                    />
+                    {item.descr?.resume ? <h3>{item.descr?.resume}</h3> : null }
+                    {item.descr?.email ? <p><FontAwesomeIcon size="lg" icon={ faEnvelope }/> {item.descr?.email}</p> : null }
+                    {item.descr?.badge ? <p><FontAwesomeIcon size="lg" icon={faIdBadge} className="members-icon" alt="linkedin"/> {item.descr?.badge}</p> : null }
+                    {item.descr?.page ? <a href={item.descr?.page}><FontAwesomeIcon size="lg" icon={ faWindowMaximize }/> {item.descr?.page}</a> : null }
+                    <div className="media">
+                        {item.descr?.lattes ? <a href={item.descr?.lattes}><button className="btn lattes-btn">Lattes</button></a> : null }
+                        {item.descr?.github ? <a href={item.descr?.github}><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" className="media-icon" alt="github"/> </a> : null }
+                        {item.descr?.linkedin ? <a href={item.descr?.linkedin}><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" className="media-icon" alt="linkedin"/> </a> : null }
+                    </div>
+                </div>
             </div>
-            
-        </div>
-    );
+            </>
+        );
+    } else {
+        return (
+            <div className="flex-image">
+                <div onClick={ setShowInfo } className="flex-image-info">
+                    <img src={item.avatar} alt={item.name}/>
+                    <h4>{item.name}</h4>
+                </div>
+            </div>
+        )
+    }
 
 }
 
--- src/components/members/members.js
+++ src/components/members/members.js
@@ -1,16 +1,18 @@
 import FlexImages from './components/FlexImages/flexImages'
 
-import { teachers } from './membersList.js'
-
-import './members.css'
+import { teachers, students, exStudents } from './membersList.js'
 
 export default function Members(){
 
     return (
         <section className="members">
-            <div className="container">
+            <div className="container members-content">
                 <h1>Members</h1>
                 <FlexImages list={teachers}/>
+                <FlexImages list={students}/>
+
+                <h2>Ex-Members</h2>
+                <FlexImages list={exStudents}/>
             </div>
         </section>
     )

86766119504f0b76b17f7a88992846658a1393e8
jonatas <jonatasgeek@gmail.com>
2021-08-04 02:51:20 -0300

    Adiciona um menu responsivo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
+
 import logo from '../../assets/img/logo/logo_knedle.svg';
 import brFlag from '../../assets/img/iconBrasil.png'
 import usFlag from '../../assets/img/iconUK.png'
 import githubLogo from '../../assets/img/iconGithub.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendarWeek, faBookOpen, faTools, faUsers, faHandshake } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarWeek, faBookOpen, faTools, faUsers, faHandshake, faBars } from '@fortawesome/free-solid-svg-icons'
 
+import './sidebar.css'
 import './header.css'
 
 export default function Header(props){
     var background = props.bg
+
+    const [showMenu, setShowMenu] = useState(false);
 
     return (
         <header className={background ? "navbar" : "navbar no-bg"}>
@@ -43,8 +48,44 @@ export default function Header(props){
                         <a className="navbar-flag" href="."><img src={usFlag} className="navbar-img" alt="flag"/></a>
                         <a className="navbar-flag" href="."><img src={brFlag} className="navbar-img" alt="flag"/></a>
                     </div>
+                    <div className={showMenu ? "sidebar active" : "sidebar"}>
+                        <button
+                            onClick={() => setShowMenu(!showMenu) }
+                            className={showMenu ? "sidebar-item active bars" : "sidebar-item bars"}
+                        >
+                            <FontAwesomeIcon size="lg" icon={faBars} />
+                        </button>
+                        <a className="sidebar-item" href=".">
+                            <FontAwesomeIcon icon={faCalendarWeek} />
+                            Events
+                        </a>
+                        <a className="sidebar-item" href=".">
+                            <FontAwesomeIcon icon={faBookOpen} />
+                            Publications
+                        </a>
+                        <a className="sidebar-item" href=".">
+                            <FontAwesomeIcon icon={faTools} />
+                            Toolbox
+                        </a>
+                        <a className="sidebar-item" href=".">
+                            <FontAwesomeIcon icon={faUsers} />
+                            Members
+                        </a>
+                        <a className="sidebar-item" href=".">
+                            <FontAwesomeIcon icon={faHandshake} />
+                            Partners
+                        </a>
+                        <a className="sidebar-item github-logo" href=".">
+                            <img src={githubLogo} alt="github-logo"/>
+                        </a>
+                        <div className="sidebar-flags">
+                            <a className="sidebar-flag" href="."><img src={usFlag} className="navbar-img" alt="flag"/></a>
+                            <a className="sidebar-flag" href="."><img src={brFlag} className="navbar-img" alt="flag"/></a>
+                        </div>
+                    </div>
                 </div>
             </div>
+            
         </header>
     )
 }

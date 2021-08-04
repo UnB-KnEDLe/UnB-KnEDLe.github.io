4e8c3691a177629f597ccd9b1d0c0f15becbf606
jonatas <jonatasgeek@gmail.com>
2021-08-04 00:11:05 -0300

    Finaliza Publicações. Estilo e conteúdo

--- src/sections/publications/publications.js
+++ src/sections/publications/publications.js
@@ -6,7 +6,7 @@ import './publications.css'
 
 export default function Publications(){
     const [showAll, setShowAll] = useState(false)
-    const publicationsLimit = 4
+    const publicationsLimit = 3
 
     return (
         <section className="publications">
@@ -17,9 +17,15 @@ export default function Publications(){
                         if(index < publicationsLimit || showAll) {
                             return (
                                 <li className="publication-item" key={index}>
-                                    <h3>{item.title}</h3>
-                                    <p>{item.description}</p>
-                                    <a href={item.link.url}>{item.link.title}</a>
+                                    <h3>{item?.title}</h3>
+                                    {item?.content.map(contentEl => {return (<p>{contentEl}</p>)})}
+                                    {item?.link.map(linkEl => {
+                                        return (
+                                            <a href={linkEl.url}>
+                                                <button className="btn">{linkEl.title}</button>
+                                            </a>
+                                        )
+                                    })}
                                 </li>
                             )
                         } else return null
--- src/sections/publications/publicationsList.js
+++ src/sections/publications/publicationsList.js
@@ -1,54 +1,78 @@
 const publicationsList = [
     {
-        title: 'Publication 1',
-        description: 'Description 1',
-        link: {title: 'Google', url: 'http://www.google.com'},
+        title: 'KnEDLe/NIDO Project Partial Technical Report 2',
+        content: ['• Isaque Alves • Vinícius R. P. Borges • Thiago de Paulo Faleiros • Pedro H. Luz de Araujo • Frederico Guth • Lívia Fonseca • Tatiana Pereira • Carolina Alves Okimoto • Teófilo E. de Campos'],
+        link: [{title: 'PDF', url: 'https://cic.unb.br/~teodecampos/KnEDLe/reports/knedle_release_2.pdf'}],
     },
     {
-        title: 'Publication 2',
-        description: 'Description 2',
-        link: {title: 'Google', url: 'http://www.google.com'},
-    },
-        {
-        title: 'Publication 3',
-        description: 'Description 3',
-        link: {title: 'Google', url: 'http://www.google.com'},
-    },
-        {
-        title: 'Publication 4',
-        description: 'Description 4',
-        link: {title: 'Google', url: 'http://www.google.com'},
-    },
-        {
-        title: 'Publication 5',
-        description: 'Description 5',
-        link: {title: 'Google', url: 'http://www.google.com'},
-    },
-        {
-        title: 'Publication 6',
-        description: 'Description 6',
-        link: {title: 'Google', url: 'http://www.google.com'},
-    },
-        {
-        title: 'Publication 7',
-        description: 'Description 7',
-        link: {title: 'Google', url: 'http://www.google.com'},
-    },
-        {
-        title: 'Publication 8',
-        description: 'Description 8',
-        link: {title: 'Google', url: 'http://www.google.com'},
+        title: 'Topic Modelling Brazilian Supreme Court Lawsuits',
+        content: [
+            '• Pedro H. Luz de Araujo • Teófilo E. de Campos',
+            '33rd International Conference on Legal Knowledge and Information Systems (JURIX 2020)'
+        ],
+        link: [{title: 'PDF', url: 'http://nido.unb.br/assets/papers/8_Araujo.pdf'}],
     },
     {
-        title: 'Publication 9',
-        description: 'Description 9',
-        link: {title: 'Google', url: 'http://www.google.com'},
+        title: 'VisNote: A Tool For Interactive Visual Exploration And Text Annotation Of Government Gazettes',
+        content: ['• Tatiana F. Pereira • Lívia G. C Fonseca • Matheus S. V. de Oliveira • Teofilo E. Campos • Vinícius R. P. Borges', '33rd International Conference on Graphics, Patterns and Images (SIBGRAPI 2020)'],
+        link: [{title: 'Abstract', url: 'http://nido.unb.br/assets/papers/VisNote_Abstract.pdf'}],
     },
-        {
-        title: 'Publication 10',
-        description: 'Description 10',
-        link: {title: 'Google', url: 'http://www.google.com'},
+    {
+        title: 'Topic Modelling Brazilian Supreme Court Lawsuits',
+        content: ['• Pedro H. Luz de Araujo • Teófilo E. de Campos', '33rd International Conference on Legal Knowledge and Information Systems (JURIX 2020)'],
+        link: [{title: 'PDF', url: 'http://nido.unb.br/assets/papers/8_Araujo.pdf'}],
+    },
+    {
+        title: 'Inferring The Source Of Official Texts: Can SVM Beat ULMFiT?',
+        content: ['• Pedro H. Luz de Araujo • Teófilo E. de Campos • Marcelo M. Silva de Sousa', 'PROPOR 2020 - International Conference on the Computational Processing of Portuguese'],
+        link: [
+            {title: 'Bibtex', url: 'https://cic.unb.br/~teodecampos/KnEDLe/propor2020/luz_de_araujo_etal_propor2020.bib'},
+            {title: 'PDF', url: 'https://cic.unb.br/~teodecampos/KnEDLe/propor2020/luz_de_araujo_etal_propor2020.pdf'},
+            {title: 'All', url: 'https://cic.unb.br/~teodecampos/KnEDLe/propor2020/'}
+        ],
+    },
+    {
+        title: 'Victor: A Dataset For Brazilian Legal Documents Classification',
+        content: ['• Pedro Henrique Luz de Araujo • Teófilo Emídio de Campos • Fabricio Ataides Braz • Nilton Correia da Silva', 'Language Resources and Evaluation Conference (LREC)'],
+        link: [
+            {title: 'Bibtex', url: 'https://cic.unb.br/~teodecampos/ViP/lrec/luz_etal_victor_lrec2020.bib'},
+            {title: 'PDF', url: 'https://cic.unb.br/~teodecampos/ViP/lrec/luz_etal_lrec2020.pdf'},
+            {title: 'All', url: 'https://cic.unb.br/~teodecampos/ViP/lrec/'}
+        ],
+    },
+    {
+        title: 'KnEDLe/NIDO Project Partial Technical Report 1',
+        content: ['• Teófilo E. de Campos • Thiago de Paulo Faleiros • Vinícius R. P. Borges • Isaque Alves • Carolina Alves Okimoto'],
+        link: [{title: 'PDF', url: 'https://cic.unb.br/~teodecampos/KnEDLe/reports/knedle_release_1.pdf'}],
+    },
+    {
+        title: 'The Information Bottleneck Theory Of Deep Learning',
+        content: [
+            'MSc qualification exam',
+            'Author • Frederico Guth',
+            'Supervisor • Teófilo E. de Campos, University of Brasilia',
+            'Committee • John Shawe-Taylor (UCL) • Moacir Ponti (USP) • Thiago de Paulo Faleiros (suplente - UnB)'
+        ],
+        link: [
+            {title: 'MSc qualification report (33.7MB)', url: 'https://cic.unb.br/~teodecampos/fred_guth/guth_msc_qualify_unb_2020.pdf'},
+            {title: 'Slides (8.5MB)', url: 'https://cic.unb.br/~teodecampos/fred_guth/guth_msc_qualify_unb_2020_slides.pdf'}
+        ]
+    },
+    {
+        title: 'From Documents To Entities: A Journey Through Natural Language Processing Tasks And Domains',
+        content: [
+            'MSc Qualification Exam',
+            'Author • Pedro Luz',
+            'Supervisor • Teófilo E. de Campos, University of Brasilia',
+            'Committee • Alexandre Rademaker (FGV and IBM) • Thiago de Paulo Faleiros (UnB)'
+        ],
+        link: [
+            {title: 'MSc qualification report (882KB)', url: 'https://cic.unb.br/~teodecampos/peluz/luz_de_araujo_MSc_qualify.pdf'},
+            {title: 'Slides (1.5MB)', url: 'https://cic.unb.br/~teodecampos/peluz/luz_de_araujo_MSc_qualify_slides.pdf'},
+            {title: 'All', url: 'https://cic.unb.br/~teodecampos/peluz/'}
+        ]
     },
 ]
 
 export default publicationsList;
+

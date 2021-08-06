2b25ef4ba8acc6caf67378326f734451c8acb867
jonatas <jonatasgeek@gmail.com>
2021-08-06 11:41:14 -0300

    Adiciona animação diferente a frontpage

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Header from '../../components/header/header'
 import FrontPage from '../../sections/frontpage/frontpage'

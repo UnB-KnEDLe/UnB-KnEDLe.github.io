78e02cf88a7f63a43ed1417f52b5e560f3cfade7
jonatas <jonatasgeek@gmail.com>
2021-08-10 13:15:58 -0300

    Modificação em app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch
-} from 'react-router-dom'
-
 import './App.css';
 
 import Home from './pages/home/home'
-import Publications from './pages/publications/publications'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={'/'} component={Home}/>
-        <Route exact path={'/publications'} component={Publications}/>
-        <Redirect to={'/'} />
-      </Switch>
-    </Router>
+    <Home/>
   );
 }
 

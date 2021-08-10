c2a1e48be61ea4c14ab577d22bd2669baf334da5
jonatas <jonatasgeek@gmail.com>
2021-08-10 13:19:36 -0300

    Alterações

--- src/sections/publications/publications.js
+++ src/sections/publications/publications.js
@@ -1,10 +1,23 @@
+import { useState } from 'react'
 import publicationsList from '../../components/publicationsList/publicationsList'
+
+import Modal from 'react-modal'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import './publications.css'
 
+var style = {
+    content: {
+        marginTop: '50px',
+    }
+}
+
 export default function Publications(props){
     var t = props.translationFunction
-
+    const [showAll, setShowAll] = useState(false)
+    
     const publicationsLimit = 3
 
     return (
@@ -29,10 +42,40 @@ export default function Publications(props){
                             )
                         } else return null
                     })}
-                    <button className="btn show-all-btn">
-                        <a href="./publications" >{t("See all publications")}</a>
+                    <button onClick={ () => { setShowAll(true) }}className="btn show-all-btn">
+                        {t("See all publications")}
                     </button>
                 </ul>
+                <Modal
+                    isOpen={showAll}
+                    style={style}
+                >
+                    <div className="publications-content">
+                        <div className="publications-header">
+                            <h1>Publications</h1>
+                            <button onClick={ () => setShowAll(false) } className="btn">
+                                <FontAwesomeIcon size="lg" icon={faTimes}/>
+                            </button>
+                        </div>
+                        <ul className="publications-page-list">
+                            { publicationsList.map((item, index) => {
+                                return (
+                                    <li className="publication-page-item" key={index}>
+                                        <h3>{item?.title}</h3>
+                                        {item?.content.map(contentEl => {return (<p>{contentEl}</p>)})}
+                                        {item?.link.map(linkEl => {
+                                            return (
+                                                <a href={linkEl.url}>
+                                                    <button className="btn">{linkEl.title}</button>
+                                                </a>
+                                            )
+                                        })}
+                                    </li>
+                                )
+                            }) }
+                        </ul>
+                    </div>
+                </Modal>
             </div>
         </section>
     )

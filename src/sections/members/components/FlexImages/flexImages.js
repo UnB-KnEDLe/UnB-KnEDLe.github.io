88a361e59bd96fc207fa00e05cd86347621046aa
Jônatas G <jonatasgeek@gmail.com>
2021-11-22 17:07:44 -0300

    make link open in new tab

--- src/sections/members/components/FlexImages/flexImages.js
+++ src/sections/members/components/FlexImages/flexImages.js
@@ -34,11 +34,11 @@ function FlexImage(props) {
                     {item.descr?.resume ? <h3>{item.descr?.resume}</h3> : null }
                     {item.descr?.email ? <p><FontAwesomeIcon size="lg" icon={ faEnvelope }/> {item.descr?.email}</p> : null }
                     {item.descr?.badge ? <p><FontAwesomeIcon size="lg" icon={faIdBadge} className="members-icon" alt="linkedin"/> {item.descr?.badge}</p> : null }
-                    {item.descr?.page ? <a href={item.descr?.page}><FontAwesomeIcon size="lg" icon={ faWindowMaximize }/> {item.descr?.page}</a> : null }
+                    {item.descr?.page ? <a target="_blank" href={item.descr?.page}><FontAwesomeIcon size="lg" icon={ faWindowMaximize }/> {item.descr?.page}</a> : null }
                     <div className="media">
-                        {item.descr?.lattes ? <a href={item.descr?.lattes}><button className="btn lattes-btn">Lattes</button></a> : null }
-                        {item.descr?.github ? <a href={item.descr?.github}><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" className="media-icon" alt="github"/> </a> : null }
-                        {item.descr?.linkedin ? <a href={item.descr?.linkedin}><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" className="media-icon" alt="linkedin"/> </a> : null }
+                        {item.descr?.lattes ? <a target="_blank" href={item.descr?.lattes}><button className="btn lattes-btn">Lattes</button></a> : null }
+                        {item.descr?.github ? <a target="_blank" href={item.descr?.github}><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" className="media-icon" alt="github"/> </a> : null }
+                        {item.descr?.linkedin ? <a target="_blank" href={item.descr?.linkedin}><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" className="media-icon" alt="linkedin"/> </a> : null }
                     </div>
                 </div>
             </div>

ff3a54aeb2e0624a1ec070d6b9e4208d9433a199
Jônatas G <jonatasgeek@gmail.com>
2021-11-29 14:44:45 -0300

    update noreferrer

--- src/sections/partners/partners.js
+++ src/sections/partners/partners.js
@@ -11,13 +11,13 @@ export default function Partners(props){
             <div className="container">
                 <h2><b>{t('Partners')}</b></h2>
                 <div className="partners-img">
-                    <a target="_blank" href="https://www.unb.br/">
+                    <a target="_blank" rel="noreferrer" href="https://www.unb.br/">
                         <img src={unbLogo} className="unb-logo" alt="Universidade de Brasília" />
                     </a>
-                    <a target="_blank" href="http://www.fap.df.gov.br/">
+                    <a target="_blank" rel="noreferrer" href="http://www.fap.df.gov.br/">
                         <img src={fapLogo} alt="Fundação de Apoio à Pesquisa no Distrito Federal" />
                     </a>
-                    <a target="_blank" href="https://www.finatec.org.br/">
+                    <a target="_blank" rel="noreferrer" href="https://www.finatec.org.br/">
                         <img src={finatecLogo} alt="Fundação de Empreendimentos Científicos e Tecnológicos"/>
                     </a>
                 </div>
